2dc50f20e289442db2bb66f912fc2e43b1af78a1
Lorenzo PILOTTO <lpilotto@student.42.fr>
2016-03-13 21:32:25 +0100

    maj

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,15 +6,12 @@
 /*   By: lpilotto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 16:42:53 by lpilotto          #+#    #+#             */
-/*   Updated: 2015/11/27 16:47:55 by lpilotto         ###   ########.fr       */
+/*   Updated: 2016/03/13 21:31:34 by lpilotto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_toupper(int c)
 {
-	unsigned char ch;
-
-	ch = (unsigned char)c;
 	if (c >= 'a' && c <= 'z')
 		return ((int)(c - 'a' + 'A'));
 	return (c);
